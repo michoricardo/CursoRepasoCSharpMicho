19ba1229cca0093f76dd6517d84bea7fbd33f34f
Ricardo Garcia <ricardo.garcia@epicor.com>
2021-10-19 18:48:42 -0500

    arrays

---
+++ Leccion11_Arreglos/Leccion11_Arreglos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Leccion11_Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Para hacer un arreglo, necesitas ver que tipo de datos va a almacenar el arreglo y tambien definir su longitud
+            int[] arreglo1 = new int[4];
+            Console.WriteLine(arreglo1.Length); //para ver la longitud de ese arreglo
+            arreglo1[0] = 3;
+            arreglo1[1] = 4;
+            arreglo1[2] = 37;
+            arreglo1[3] = 30;
+
+            for (int cont = 0; cont < arreglo1.Length; cont++)
+            {
+                Console.WriteLine(arreglo1[cont]);
+            }
+
+        }
+    }
+}
