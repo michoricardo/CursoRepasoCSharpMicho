c6a68d14670af356e4d09a53d4d38e7b1c07345a
Ricardo Garcia <ricardo.garcia@epicor.com>
2021-10-20 18:51:55 -0500

    las del 20 de oct

---
+++ Leccion 13 usando mas parametros/Leccion 13 usando mas parametros/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Leccion_13_usando_mas_parametros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 1;
+            decimal num2 = (decimal)2.5; //castear el valor
+            string hola = "hola!";
+            string texto = "como estas?";
+            Console.WriteLine("Hola desde el main");
+            saludar(num1, num2, hola, texto);
+        }
+
+        static void saludar(int n1, decimal n2, string t1, string t2)
+        {
+            Console.WriteLine("el int n1:" + n1 + "\n");
+            Console.WriteLine("el decimal n2:" + n2 + "\n");
+            Console.WriteLine("el hola t1:" + t1 + "\n");
+            Console.WriteLine("el texto t2:" + t2 + "\n");
+        }
+
+    }
+}
---
+++ Leccion12_Funciones/Leccion12_Funciones/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Leccion12_Funciones
+{
+    class leccion12
+    {
+        static void Main(string[] args)//funcion principal
+        {
+            Console.WriteLine("Favor de poner su nombre\n");
+            string nombre=Console.ReadLine();
+            saludar(nombre);
+            asciiFunction();
+            int num = 3;
+            
+            //int numero = devolverNum();
+            //Console.WriteLine("Ejecutando la funcion de num: " + numero);
+            //numero sera 2
+            parametroPorValor(num);
+            Console.WriteLine("Imprimiendo parametro por valor \n" + num);
+            Console.WriteLine("---------------------------------------\n");
+            parametroPorRef(ref num);
+            Console.WriteLine("Imprimiendo parametro por referencia \n" + num);
+        }
+        static void asciiFunction()
+        {
+            Console.WriteLine(@"(             )
+ `--(_   _)--'
+      Y-Y
+     /@@ \
+    /     \
+    `--'.  \             ,
+        |   `.__________/) ");
+            Console.WriteLine("\n\n");
+        }
+        static void parametroPorValor(int num)
+        {
+            num *= 2;
+            // el valor de num se afecta solo en esta funcion
+         
+            
+        }
+        static void parametroPorRef(ref int num)
+        {
+            num *= 2;
+           
+            //el valor de num se afecta globalmente
+        }
+
+        static void saludar(string neim) { //static porque esta funcion pertenece al mismo bloque que la clase leccion12 del proyecto leccion12_funciones
+            Console.WriteLine("Hellow madafakar " + neim + "\n");
+        }
+        static int devolverNum()
+        {
+            int num1 = 1;
+            int num2 = 1;
+            int result = num1 + num2;
+            //regresa un numero
+            return result;
+        }
+    }
+}
